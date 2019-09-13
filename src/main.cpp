ce413ceaad7fd1596da23e028082bdc631a309a8
John Behm <jbehm@uos.de>
2019-09-13 12:57:44 +0200

    added debugging config

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 #include "mbed.h"
 
+// disable these LEDs
+DigitalOut led2(P1_29, 0);
+DigitalOut led3(P1_31, 0);
+DigitalOut led4(P2_2, 0);
+DigitalOut led5(P2_3, 0);
+DigitalOut led6(P2_4, 0);
+DigitalOut led7(P2_5, 0);
+DigitalOut led8(P2_6, 0);
+
+// only blink this LED
 DigitalOut myled(P1_28);
 
 int main() {
