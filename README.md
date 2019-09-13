d334b3a1740b87dd5a561de09df93b0cc58ec927
John Behm <jbehm@uos.de>
2019-09-13 19:27:40 +0200

    added some stuff

--- README.md
+++ README.md
@@ -6,7 +6,16 @@ This was constructed with the help of [maxgerhardt](https://github.com/maxgerhar
 
 ### Troubleshooting
 
+#### Remove any other installed platformIO or openOCD installation
+```
+brew remove platformio open-ocd
+```
+
 #### Python Module 'intelhex' not found
 ```
 ./.platformio/penv/bin/pip3 install intelhex
 ```
+
+#### Error: registers may not be the same -- `strexb r0,r0,[r1]' `strexh r0,r0,[r1]'
+
+Use this patch: [click me](https://gist.github.com/timbrom/1942280)
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,46 @@
 
-#include <LPC1768/LPC17xx.h>
-#include <map>
-#include <iostream>
+#include <LPC17xx/LPC17xx.h>
 
-extern "C" int main(void);
+#ifdef __cplusplus
+ extern "C" {
+#endif 
 
-void Setup()
+extern void SystemInit (void);
+
+
+void delay_ms(unsigned int ms)
 {
+    unsigned int i,j;
 
+    for(i=0;i<ms;i++)
+        for(j=0;j<20000;j++);
 }
 
-int main() {
-    Setup();
+int main(){
+    SystemInit();                    //Clock and PLL configuration 
 
-    std::map<int, int> PinMap;
+    LPC_PINCON->PINSEL4 = 0x000000;  //Configure the PORT2 Pins as GPIO;
+    LPC_GPIO2->FIODIR = 0xffffffff;  //Configure the PORT2 Pins as OUTPUT;
 
-    for (size_t i = 0; i < 512; i++)
+
+    while(1)
     {
-        PinMap.insert({i, i * i});
+        LPC_GPIO2->FIOPIN = 0xffffffff;     // Make all the Port pins as high  
+        delay_ms(100);
+
+        LPC_GPIO2->FIOPIN = 0x00;           // Make all the Port pins as low  
+        delay_ms(100);
     }
     
 }
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+ }
+#endif 
