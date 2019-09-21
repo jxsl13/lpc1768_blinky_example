09a14529e7d49d9f7c1308f40796e0aa10705228
John Behm <jbehm@uos.de>
2019-09-21 11:44:44 +0200

    nothing new, still fails.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
- 
+#include <LPC17xx/LPC17xx.h> 
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+void delay_ms(unsigned int ms)
+{
+unsigned int i,j;
+for(i=0;i<ms;i++)
+for(j=0;j<6000;j++);
+}
+
 
 // main() runs in its own thread in the OS
 int main() {
-    int i = 0;
+    SystemInit();
     
-    while(true) {
-        i++;
+    //resetBits<uint32_t>(&(LPC_PINCON->PINSEL3), {24, 25});
+    LPC_PINCON->PINSEL4 = 0x00000000;  //Configure the PORT2 Pins as GPIO;
+    LPC_GPIO2->FIODIR = 0xffffffff;
+
+    
+    while(true)
+    {
+        LPC_GPIO2->FIOSET = 0xffffffff; // Make all the Port pins as high  
+        delay_ms(200);
+        LPC_GPIO2->FIOCLR = 0xffffffff;     // Make all the Port pins as low  
+        delay_ms(200);
     }
 }
 
