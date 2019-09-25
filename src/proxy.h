9111b27ac1580776882ed38b86508c71f173e7cc
John Behm <jbehm@uos.de>
2019-09-25 17:33:09 +0200

    multi target base

--- src/proxy.h
+++ src/proxy.h
@@ -1,8 +1,11 @@
 #pragma once // include this header only once
 
 #if defined LPC1768
-#include <LPC17xx/LPC17xx.h>
+#include <LPC17xx.h>
+
 #elif defined ATMEGA328P
+
 #define BOARD ATMEGA328P_XPLAINED_MINI
-#include <ATmega328p/asf.h>
+#include <asf.h>
+
 #endif // include platform specific headers.
