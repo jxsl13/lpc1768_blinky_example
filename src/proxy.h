53bb04af9dc8f6c1d215b921d03049a8a9793ec1
John Behm <jbehm@uos.de>
2019-09-26 15:07:38 +0200

    src_filter approach, #include <xyz.h> not found

--- src/proxy.h
+++ src/proxy.h
@@ -1,11 +1,11 @@
 #pragma once // include this header only once
 
 #if defined LPC1768
-#include <LPC17xx.h>
+#include <target/LPC1768/LPC17xx.h>
 
 #elif defined ATMEGA328P
 
 #define BOARD ATMEGA328P_XPLAINED_MINI
-#include <asf.h>
+#include <target/ATMEGA328P/asf.h>
 
 #endif // include platform specific headers.
