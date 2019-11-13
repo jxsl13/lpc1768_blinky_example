79c8e2b0e9f685b5633a575c3e4d74b93a59294e
jxsl13 <jxsl13@gmail.com>
2019-11-13 19:56:27 +0100

    add config size.

--- src/target/STM32F407VG/InterruptVectorTable.cpp
+++ src/target/STM32F407VG/InterruptVectorTable.cpp
@@ -9,7 +9,7 @@ typedef void(*pFn_t)(void);
 
 
 const ValidIRQTypes InterruptVectorTable::s_ValidIRQTypes = {
-    82,
+    ValidInterruptCount,
     { 
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 
         10, 11, 12, 13, 14, 15, 16, 17, 18, 
