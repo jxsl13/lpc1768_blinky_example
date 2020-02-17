50e1e52634599394cf45f5e77333b89390b39673
jxsl13 <jxsl13@gmail.com>
2020-02-17 19:36:30 +0100

    fix indentatins and comments

--- src/main.cpp
+++ src/main.cpp
@@ -64,39 +64,38 @@ int main()
 
 void InitGPIO()
 {
-    SystemInit();                       // startup board in a predefined state
+    SystemInit();                    // startup board in a predefined state
 
     // LED - P1.28 - Pin Function
-    DISABLE(LPC_PINCON->PINSEL3, 24);   // P1.28 - AS GPIO Pin
-    DISABLE(LPC_PINCON->PINSEL3, 25);   // ...(Manual Pages 114, 119, 120)
+    DISABLE(LPC_PINCON->PINSEL3, 24);// P1.28 AS GPIO Pin
+    DISABLE(LPC_PINCON->PINSEL3, 25);// ...(Manual Pages 114, 119, 120)
 
     // LED - P1.28 - GPIO Configuration
-    ENABLE(LPC_GPIO1->FIODIR, 28);      // P1.28 - As output (Manual Page 133)
-    DISABLE(LPC_GPIO1->FIOPIN, 28);      // P1.28 - Set as LOW (Manual Pages 132,150)
+    ENABLE(LPC_GPIO1->FIODIR, 28); // P1.28 As output(Manual Page 133)
+    DISABLE(LPC_GPIO1->FIOPIN, 28);// P1.28 Set as LOW(Manual Pages 132,150)
 
     // Pushbutton - P2.10 (INT0)
-    DISABLE(LPC_PINCON->PINSEL4, 21);   // P2.10 - As external Interrupt (EINT0) 
-    ENABLE(LPC_PINCON->PINSEL4, 20);    // ...(Manual Page 120)
-
-    DISABLE(LPC_GPIO2->FIODIR, 10);     // P2.10 - As input (Manual Page 133)
+    DISABLE(LPC_PINCON->PINSEL4, 21);// P2.10 As external Interrupt(EINT0) 
+    ENABLE(LPC_PINCON->PINSEL4, 20); // ...(Manual Page 120)
 }
 
 void InitExtInt0()
 {
-    __disable_irq();                // cmsis_gcc.h - Disable interrupts globally
+    __disable_irq();             // cmsis_gcc.h - Disable interrupts globally
 
-    ENABLE(LPC_SC->EXTINT, 0);      // EINT0 - Clear pending bit (Manual Pages 26,27)
-    ENABLE(LPC_SC->EXTMODE, 0);     // EINT0 - Edge sensitive (Manual Page 26, 28)
-    DISABLE(LPC_SC->EXTPOLAR, 0);    // EINT0 - Falling edge (Manual Page 26,28)
+    ENABLE(LPC_SC->EXTINT, 0);   // EINT0 Clear pending bit(Manual Pages 26,27)
+    ENABLE(LPC_SC->EXTMODE, 0);  // EINT0 Edge sensitive(Manual Page 26, 28)
+    DISABLE(LPC_SC->EXTPOLAR, 0);// EINT0 Falling edge(Manual Page 26,28)
 
-    NVIC_EnableIRQ(EINT0_IRQn);     // LPC17xx.h - Enable external Interrupt (EINT0)
-    __enable_irq();                 // cmsis_gcc.h - Enable interrupts globally
+    NVIC_EnableIRQ(EINT0_IRQn);// LPC17xx.h - Enable external Interrupt(EINT0)
+    __enable_irq();            // cmsis_gcc.h - Enable interrupts globally
 }
 
 void delay_ms(unsigned int ms)
 {
-    // Not necessarily accurate, but enought of a delay for testing purposes
-    unsigned int resetValue = 8 * (SystemFrequency / (SystemFrequency / 1000)); // system_LPC17xx.h
+    // Not necessarily accurate, but enough of a delay for testing purposes
+    // SystemFrequency is defined in system_LPC17xx.h
+    unsigned int resetValue = 8 * (SystemFrequency / (SystemFrequency / 1000));
     while (ms-- > 0)
     {
         unsigned int x = resetValue;
@@ -107,13 +106,13 @@ void delay_ms(unsigned int ms)
 
 void ToggleLED()
 {
-    TOGGLE(LPC_GPIO1->FIOPIN, 28);  // P2.10 - Toggle state (Manual Page 133)
+    TOGGLE(LPC_GPIO1->FIOPIN, 28);// P2.10 Toggle state (Manual Page 133)
 }
 
-extern "C" void EINT0_IRQHandler()  // Definitions in startup_LPC17xx.S
+extern "C" void EINT0_IRQHandler()// Definitions in startup_LPC17xx.S
 {
     ToggleLED();
-    ENABLE(LPC_SC->EXTINT, 0);      // clear pending flag
+    ENABLE(LPC_SC->EXTINT, 0);    // clear pending flag
 }
 
 int main()
@@ -122,7 +121,6 @@ int main()
     InitExtInt0();  // configure external interrupt
     while(true){};
 }
-
 #elif defined STM32F407VG
 #include "controllers/stm32f407vg/stm32f4xx.h"
 
