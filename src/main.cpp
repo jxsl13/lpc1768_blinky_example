226f5087a9ffbd1128af0c7f2bb1cd555122ce4b
jxsl13 <jxsl13@gmail.com>
2020-02-17 14:27:37 +0100

    testing lpc1768

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#if defined ATMEGA328P
+
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <utils/BitMacros.hpp>
@@ -14,7 +16,7 @@ void InitGPIO()
 
     // Make Port D2 Input External Interrupt
     DISABLE(DDRD, DDD2);    // set PD2 as input
-    ENABLE(PORTD, PORTD2);  // trie-state
+    ENABLE(PORTD, PORTD2);  // tri-state
 }
 
 void InitExtInt0()
@@ -39,14 +41,14 @@ void delay_ms(unsigned int ms)
     }
 }
 
-ISR(INT0_vect)
+ISR(INT0_vect, ISR_NOBLOCK)
 {
     for (int i = 0; i < 2; i++) 
     {
         ToggleLED();
         delay_ms(300);
     }
-    ENABLE(EIFR, 0);    // clear pending flag (Manual Page 81)
+    //ENABLE(EIFR, INTF0);    // clear pending flag (Manual Page 81)
 }
 
 int main()
@@ -56,3 +58,80 @@ int main()
     while(true){/* do nothing */};
 }
 
+#elif defined LPC1768
+#include "controllers/lpc1768/LPC17xx.h"
+#include <utils/BitMacros.hpp>
+
+void InitGPIO()
+{
+    SystemInit();                       // startup board in a predefined state
+    ENABLE(LPC_SC->PCONP, 15);          // enable power (Manual Page 65)
+
+    // LED - P1.28 - Pin Function
+    DISABLE(LPC_PINCON->PINSEL3, 24);   // P1.28 - AS GPIO Pin
+    DISABLE(LPC_PINCON->PINSEL3, 25);   // ...(Manual Pages 114, 119, 120)
+
+    // LED - P1.28 - GPIO Configuration
+    ENABLE(LPC_GPIO1->FIODIR, 28);      // P1.28 - As output (Manual Page 133)
+    DISABLE(LPC_GPIO1->FIOPIN, 28);      // P1.28 - Set as LOW (Manual Pages 132,150)
+
+    // Pushbutton - P2.10 (INT0)
+    DISABLE(LPC_PINCON->PINSEL4, 21);   // P2.10 - As external Interrupt (EINT0) 
+    ENABLE(LPC_PINCON->PINSEL4, 20);    // ...(Manual Page 120)
+
+    DISABLE(LPC_GPIO2->FIODIR, 10);     // P2.10 - As input (Manual Page 133)
+    ENABLE(LPC_GPIOINT->IO2IntEnF, 10); // P2.10 - Enable for Falling Edge (Manual Page 133)
+}
+
+void InitExtInt0()
+{
+    __disable_irq();                // cmsis_gcc.h - Disable interrupts globally
+
+    ENABLE(LPC_SC->EXTINT, 0);      // EINT0 - Clear pending bit (Manual Pages 26,27)
+    ENABLE(LPC_SC->EXTMODE, 0);     // EINT0 - Edge sensitive (Manual Page 26, 28)
+    ENABLE(LPC_SC->EXTPOLAR, 0);    // EINT0 - Falling edge (Manual Page 26,28)
+
+    NVIC_EnableIRQ(EINT0_IRQn);     // LPC17xx.h - Enable external Interrupt (EINT0)
+    __enable_irq();                 // cmsis_gcc.h - Enable interrupts globally
+}
+
+void ToggleLED()
+{
+    TOGGLE(LPC_GPIO1->FIOPIN, 28);  // P2.10 - Toggle state (Manual Page 133)
+}
+
+void delay_ms(unsigned int ms)
+{
+    // Not necessarily accurate, but enought of a delay for testing purposes
+    volatile unsigned int resetValue = 8 * (SystemFrequency / (SystemFrequency / 1000)); // system_LPC17xx.h
+    while (ms-- > 0)
+    {
+        volatile unsigned int x = resetValue;
+
+        while (x-- > 0)
+            __asm("nop");
+    }
+
+}
+
+void EINT0_IRQHandler() // Definitions in startup_LPC17xx.S
+{
+    ToggleLED();
+    ENABLE(LPC_SC->EXTINT, 0); // clear pending flag
+}
+
+int main()
+{
+    InitGPIO();
+    InitExtInt0();
+    while(true){
+        delay_ms(1000);
+        ToggleLED();
+    };
+}
+
+
+#elif defined STM32F407VG
+#include "controllers/stm32f407vg/stm32f4xx.h"
+
+#endif
