ff53ef44e4a07bcd3bc2e213cb17e63b04f6764b
jxsl13 <jxsl13@gmail.com>
2020-03-26 18:26:54 +0100

    arduino minimal example

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,23 @@
-/**
- * @brief These are being implemented in _example_impl.cpp
- *      for each target specifically.
- * @param int 
- */
-extern void delay_ms(unsigned int);
-extern void ToggleLED();
-extern void DisableLED();
 
-// clears external interrupt pending flag
-extern void ClearIRQCondition();
+#include "Arduino.h"
 
 
-void Blinking(unsigned int times = 1, unsigned int ms = 300)
-{
-    for (unsigned int i = 0; i < times * 2; i++)
-    {
-        delay_ms(ms);
-        ToggleLED();
-    } 
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+
+void blink() {
+  state = !state;
 }
 
-void PushButton_Handler()
-{
-    ClearIRQCondition();
+void loop() {
+  digitalWrite(ledPin, state);
 }
 
 
-int main()
-{   
-    
-    DisableLED();
+void setup() {
+  pinMode(ledPin, OUTPUT);
 
-    while(1)
-    {
-        Blinking(2);
-    } 
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
 }
-
-
