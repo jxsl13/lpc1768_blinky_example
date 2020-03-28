308f7084985702fe47a8b3d691070c9ed446e82d
jxsl13 <jxsl13@gmail.com>
2020-03-28 16:03:33 +0100

    example mbed framework (not working)

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
+#include "mbed.h"
 
-#include "Arduino.h"
+/** 
+ * does not necessarily work, but enough for a overhead demonstration
+ */
 
+#if defined LPC1768
+    constexpr PinName LED = PinName::P1_28;
+    constexpr PinName BUTTON = PinName::P2_10;
+#elif defined STM32F407VG
+    constexpr PinName LED = PinName::PD_12;
+    constexpr PinName BUTTON = PinName::PA_0;
+#endif
+ 
+DigitalOut led1(LED);
+InterruptIn btn(BUTTON);
+ 
 
-const byte ledPin = 13;
-const byte interruptPin = 2;
-volatile byte state = LOW;
-
-void blink() {
-  state = !state;
+void blink_led1() {
+    led1 = !led1;
 }
+ 
+ 
+int main() {
+    btn.fall(callback(&blink_led1));
+    btn.enable_irq();
 
-void loop() {
-  digitalWrite(ledPin, state);
-}
-
-
-void setup() {
-  pinMode(ledPin, OUTPUT);
-
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, CHANGE);
+    while(1) {}
 }
